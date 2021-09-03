800fa87d7ab014ceb3dc70409f2271d3760ca622
Juraj Puchký <jpuchky@paralix.baba.bj>
2021-09-03 23:42:26 +0200

    curl

--- src/SitemapCrawler.php
+++ src/SitemapCrawler.php
@@ -51,6 +51,7 @@ class SitemapCrawler
         curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
 
         $result = curl_exec($ch);
         curl_close($ch);
